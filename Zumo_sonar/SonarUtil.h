eb3ce1ecd83baecada4a0af8a12917ff23acea1f
Shergot <martieh@stud.ntnu.no>
2019-03-14 13:00:21 +0100

    Sonar
    Prøvde meg på noen cpp og h filer til sonar

---
+++ Zumo_sonar/SonarUtil.h
@@ -0,0 +1,21 @@
+#ifndef _sonar_util_h_
+#define _sonar_util_h_
+
+#include <Arduino.h>
+#include <NewPing.h>
+
+/*
+Informasjon fra Sonar
+---------------------
+Gir informasjon knyttet til sonaren
+-Avstand som den gir
+
+
+*/
+
+const int echoPin;
+const int triggerPin;
+int maxDistance;
+
+float distance();
+
