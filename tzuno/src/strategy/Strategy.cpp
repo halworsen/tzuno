7033839fac484a7f1220c8ac03b0751b8c21554d
Andreas Berg <andreaskb98@gmail.com>
2019-03-14 15:29:36 +0100

    fix

--- tzuno/src/strategy/Strategy.cpp
+++ tzuno/src/strategy/Strategy.cpp
@@ -6,3 +6,7 @@ Strategy::~Strategy() {}
 
 
 void Strategy::setSonarDistance(float d) { sonarDistance = d > 0 ? d : 1000; }
+
+void Strategy::setBorderLeft(bool l) { this->borderLeft = l; }
+	
+void setBorderRight(bool r) { this->borderRight = r; }
